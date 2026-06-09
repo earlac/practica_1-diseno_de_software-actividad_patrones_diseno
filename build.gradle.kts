fb0ccd3c637fd1b9a5b6e4197acfafae7152bf2a
earlac <62187974+earlac@users.noreply.github.com>
2026-06-08 18:49:52 -0600

    paso-1: implemento metodo fabrica

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.23"
+    application
 }
 
 group = "io.github.kevinah95"
@@ -11,4 +12,8 @@ repositories {
 
 kotlin {
     jvmToolchain(17)
+}
+
+application {
+    mainClass.set("patterns.creational.FactoryMethodKt")
 }
